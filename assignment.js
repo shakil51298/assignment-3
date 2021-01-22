cd609e03efa26a792b18cfaf075fd7a151bcb17b
shakil51298 <zikaiwen02@gmail.com>
2021-01-22 22:23:57 +0800

    50%

--- assignment.js
+++ assignment.js
@@ -1,4 +1,7 @@
-//---------------------------------------- Problem No :1
+// https://github.com/shakil51298/assignment-3
+
+
+//----------------------------------------------------------- Problem No :1
 
 function kilometerToMeter(Km) {
     var M = Km * 1000;
@@ -9,39 +12,50 @@ console.log(resulFor2);
 var resultFor10 = kilometerToMeter(10);
 console.log(resultFor10);
 
-// -------------------------------------Problem NO : 2
-// function budgetCalculator(clock , phone, laptop){
-//     var result = clock * 50 + phone*100 +laptop*500 ;
-//     return result;
-// }
-// var totalBudgetNeed = budgetCalculator(5,5,5);
-// console.log(totalBudgetNeed);
+// --------------------------------------------------------------Problem NO : 2
+function budgetCalculator(clock , phone, laptop){
+    var result = clock * 50 + phone*100 +laptop*500 ;
+    return result;
+}
+var totalBudgetNeed = budgetCalculator(5,5,5);
+console.log(totalBudgetNeed);
 
-
-
-// function factorial(n) {
-//     if (n ==
-//          0) {
-//         return 1;
-//     } 
-//     else {
-//         return n * factorial(n - 1);
-//     }
-// }
-// var result = factorial(5);
-// console.log(result);
-
-
-
-function fibonacci(n) {
-    if (n <=10) {
-        return n*100;
+// ---------------------------------------------------------Problem No: 3 hotelCost
+function hotelCost(days){
+    if(days<=10){
+        return days*100;
     }
-    else if (n <=20) {
-        return n*100/20;
-    } else {
-        return n*50;
+    else if(days<=20){
+        var for10Days = 10*100;
+        var left = days-10;
+        var for10To20Days = left*80;
+        total= for10Days+for10To20Days;
+        return total;
+    }
+    else{
+        var for10Days = 10 * 100;
+        var for10To20Days = 10*80;
+        var left = days - 20;
+        var forMoreThen20Days = left*50;
+        total = for10Days+for10To20Days+forMoreThen20Days;
+        return total;
     }
 }
-var result = fibonacci(21);
+var totalCost = hotelCost(71);
+console.log(totalCost);
+
+
+
+//-----------------------------------------------------problem no: 04 megaFriend
+
+function megaFriend([a,b,c]){
+    var mega = arguments[0];
+    for(i=0;i<arguments.length;i++){
+        if(arguments[i].length> mega.length){
+            mega = arguments[i];
+        }
+    }
+    return mega;
+}
+var result = megaFriend('shakil','azad','raklib','Thuhin','iqbal',' jhankar');
 console.log(result);
