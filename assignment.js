1db384bf71ca96ff9782b1152f010d03d9895ea5
shakil51298 <zikaiwen02@gmail.com>
2021-01-22 22:40:25 +0800

    80%

--- assignment.js
+++ assignment.js
@@ -13,31 +13,31 @@ var resultFor10 = kilometerToMeter(10);
 console.log(resultFor10);
 
 // --------------------------------------------------------------Problem NO : 2
-function budgetCalculator(clock , phone, laptop){
-    var result = clock * 50 + phone*100 +laptop*500 ;
+function budgetCalculator(clock, phone, laptop) {
+    var result = clock * 50 + phone * 100 + laptop * 500;
     return result;
 }
-var totalBudgetNeed = budgetCalculator(5,5,5);
+var totalBudgetNeed = budgetCalculator(5, 5, 5);
 console.log(totalBudgetNeed);
 
 // ---------------------------------------------------------Problem No: 3 hotelCost
-function hotelCost(days){
-    if(days<=10){
-        return days*100;
+function hotelCost(days) {
+    if (days <= 10) {
+        return days * 100;
     }
-    else if(days<=20){
-        var for10Days = 10*100;
-        var left = days-10;
-        var for10To20Days = left*80;
-        total= for10Days+for10To20Days;
+    else if (days <= 20) {
+        var for10Days = 10 * 100;
+        var left = days - 10;
+        var for10To20Days = left * 80;
+        total = for10Days + for10To20Days;
         return total;
     }
-    else{
+    else {
         var for10Days = 10 * 100;
-        var for10To20Days = 10*80;
+        var for10To20Days = 10 * 80;
         var left = days - 20;
-        var forMoreThen20Days = left*50;
-        total = for10Days+for10To20Days+forMoreThen20Days;
+        var forMoreThen20Days = left * 50;
+        total = for10Days + for10To20Days + forMoreThen20Days;
         return total;
     }
 }
@@ -48,14 +48,14 @@ console.log(totalCost);
 
 //-----------------------------------------------------problem no: 04 megaFriend
 
-function megaFriend([a,b,c]){
-    var mega = arguments[0];
-    for(i=0;i<arguments.length;i++){
-        if(arguments[i].length> mega.length){
-            mega = arguments[i];
+function megaFriend() {
+    var friends = arguments[0];
+    for (i = 0; i < arguments.length; i++) {
+        if (arguments[i].length > friends.length) {
+            friends = arguments[i];
         }
     }
-    return mega;
+    return friends;
 }
-var result = megaFriend('shakil','azad','raklib','Thuhin','iqbal',' jhankar');
+var result = megaFriend('Md shakil', 'Md azad', 'Md raklib', 'Thuhin', 'iqbal');
 console.log(result);
